2a189e7172d6ba4d98d9e3ffb9fc7a17ac83ad00
Maria <claudmaria1988@gmail.com>
2023-10-13 18:39:26 +0800

    add index, script, and json

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ var programmingLanguages = [
   "Rust",
   "C#"
 ];
+
+//Log out the programmingLanguages
+console.log(programmingLanguages);
+
+//for...of loops
+for (var pl of programmingLanguages){
+  console.log(`I want to learn ${pl}`);
+}
+
+//foreach
+programmingLanguages.forEach(function(pl, index){
+  var newpl = pl.toUpperCase();
+  console.log(`${index+1}. ${newpl}`);
+});
+
+//filter
+var updatedLanguange = programmingLanguages.filter(function(item){
+  return item.includes("y");
+});
+
+//Log out the updatedLanguanges
+console.log(updatedLanguange);
